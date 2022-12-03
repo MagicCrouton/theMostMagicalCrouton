d3351add45d37b741c2cb5c15dca2469cdae3e85
Eugene Park <e.park5336@gmail.com>
2022-12-03 17:03:36 -0500

    api connection with dalle/openAI is now working i've created a function that can call an image based on payLoad

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,41 @@ var pageOneEl = $('#pageOne');
 var pageTwoEl = $('#pageTwo');
 var storySubmitBtnEl = $('#storySubmit');
 var payLoad = [];
+var APIResponse=[];
+var apiKey = `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`
+// API function
 
+function fetchDallE(payLoad, i) {
+    fetch("https://api.openai.com/v1/images/generations", {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${apiKey}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "prompt": `${payLoad}`,
+            "n": 1,
+            "size": `256x256`,
+            "response_format": 'url'
+        })
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(data=>{
+        APIResponse =  data;
+        console.log('done')
+    })
+        .catch(error => {
+            console.log(error)
+        });
+
+        // use APIResponse.data[0].url to grab image url.
+}
+
+// the fetch function retrieves an image and you need to use APIResponse.data[0].url to grab image url.
+
+// end API function
 
 setTimeout(function(){
 
@@ -54,7 +88,7 @@ for (i=0; i < storyGen.length; ++i){
 
 var storySubmitBtnEl = $('#storySubmit')
 storySubmitBtnEl.on('click', function(event) {
-    event.preventDefault()
+    event.preventDefault(event);
     for (i=0; i < storyGen.length; ++i) {
 
         var tempPayload1 = $(`#payLoadA${i}`).val();
@@ -64,4 +98,7 @@ storySubmitBtnEl.on('click', function(event) {
         payLoad[i] = `${tempPayload1} ${tempPayload2}`
         }
     })
+
+
+
 }, 5000)
