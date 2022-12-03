62aa909d09bef6b3dddadd2f4a470b9d06f175b1
Eugene Park <e.park5336@gmail.com>
2022-12-03 11:09:02 -0500

    added story display iterator skeleton

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,18 @@ var storyFormSix = $('#storyFormSix').val();
 var storyFormSeven = $('#storyFormSeven').val();
 var storyFormEight = $('#storyFormEight').val();
 
+var stories = [];
+var story1 = {
+    tile1:`you find yourself standing in front of * and wanting to`,
+    tile2:`you ask * and do.`,
+    tile3:`blurb 1 * asdfasdf 2.`,
+    tile4:`blub 3 * asdfasdf`
+    };
+
+stories[0]= story1;
+
+var temp = stories[0].tile1.split("*");
+
+for (i=0; i<temp.length ; ++i){
+    story1.append(`temp[1]<span id=payload1 to dalle></span>`)
+}
