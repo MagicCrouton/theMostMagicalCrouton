5a8aa1f82ce5b08ddc66158d6eb539edb12f9e8a
Tim Aspesberro <taspes@gmail.com>
2022-12-03 10:27:03 -0500

    tims first commit

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,13 @@
-var storyOne = $('.storyOne');
+var storyOne = $('#storyOne');
+var storyTwo = $('#storyTwo');
+var storyThree = $('#storyThree');
+var storyFour = $('#storyFour');
+
+var storyFormOne = $('#storyFormOne').val();
+var storyFormTwo = $('#storyFormTwo').val();
+var storyFormThree = $('#storyFormThree').val();
+var storyFormFour = $('#storyFormFour').val();
+var storyFormFive = $('#storyFormFive').val();
+var storyFormSix = $('#storyFormSix').val();
+var storyFormSeven = $('#storyFormSeven').val();
+var storyFormEight = $('#storyFormEight').val();
