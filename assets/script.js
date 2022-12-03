619f7b4839c248c08e7b66dcdab45a55eb1e7e05
Eugene Park <e.park5336@gmail.com>
2022-12-03 17:14:46 -0500

    removed duplicate code

--- assets/script.js
+++ assets/script.js
@@ -38,71 +38,63 @@ function fetchDallE(payLoad, i) {
 
 // end API function
 
-setTimeout(function(){
-
-pageOneEl.attr('class', 'invisible');
-pageTwoEl.removeAttr('class');
 
 setTimeout(function(){
 
-pageOneEl.attr('class', 'invisible');
-pageTwoEl.removeAttr('class');
+    pageOneEl.attr('class', 'invisible');
+    pageTwoEl.removeAttr('class');
 
-var story1 = [
-`story1 you find yourself standing in front of * and wanting to`,
-`you ask * and do.`,
-`blurb 1 * asdfasdf 2.`,
-`blub 3 * asdfasdf`
-];
-
-var story2 = [
-    `story2 you find yourself standing in front of * and wanting to`,
+    var story1 = [
+    `story1 you find yourself standing in front of * and wanting to`,
     `you ask * and do.`,
     `blurb 1 * asdfasdf 2.`,
     `blub 3 * asdfasdf`
-];
+    ];
 
-var story3 = [
-    `story3 you find yourself standing in front of * and wanting to`,
-    `you ask * and do.`,
-    `blurb 1 * asdfasdf 2.`,
-    `blub 3 * asdfasdf`
-];
+    var story2 = [
+        `story2 you find yourself standing in front of * and wanting to`,
+        `you ask * and do.`,
+        `blurb 1 * asdfasdf 2.`,
+        `blub 3 * asdfasdf`
+    ];
 
-var story4 = [
-    `story4 you find yourself standing in front of * and wanting to`,
-    `you ask * and do.`,
-    `blurb 1 * asdfasdf 2.`,
-    `blub 3 * asdfasdf`
-];
+    var story3 = [
+        `story3 you find yourself standing in front of * and wanting to`,
+        `you ask * and do.`,
+        `blurb 1 * asdfasdf 2.`,
+        `blub 3 * asdfasdf`
+    ];
 
-var stories = [story1, story2, story3, story4];
+    var story4 = [
+        `story4 you find yourself standing in front of * and wanting to`,
+        `you ask * and do.`,
+        `blurb 1 * asdfasdf 2.`,
+        `blub 3 * asdfasdf`
+    ];
 
+    var stories = [story1, story2, story3, story4];
+    var randomGen = Math.floor(Math.random() * 4);
+    var storyGen = stories[randomGen];
 
+    for (i=0; i < storyGen.length; ++i){
+        var temp =storyGen[i].split("*");
+        var tempEl = $(`#story${i+1}`);
+        tempEl.text("");
+        tempEl.html(`<span>${temp[0]}<input type="text" id="payLoadA${i}">${temp[1]}<input type="text" id="payLoadB${i}"</span>`)
+    }    
 
-var randomGen = Math.floor(Math.random() * 4);
-var storyGen = stories[randomGen];
+    var storySubmitBtnEl = $('#storySubmit')
+    storySubmitBtnEl.on('click', function(event) {
+        event.preventDefault(event);
+        for (i=0; i < storyGen.length; ++i) {
 
-
-for (i=0; i < storyGen.length; ++i){
-    var temp =storyGen[i].split("*");
-    var tempEl = $(`#story${i+1}`);
-    tempEl.text("");
-    tempEl.html(`<span>${temp[0]}<input type="text" id="payLoadA${i}">${temp[1]}<input type="text" id="payLoadB${i}"</span>`)
-}    
-
-var storySubmitBtnEl = $('#storySubmit')
-storySubmitBtnEl.on('click', function(event) {
-    event.preventDefault(event);
-    for (i=0; i < storyGen.length; ++i) {
-
-        var tempPayload1 = $(`#payLoadA${i}`).val();
-        console.log(tempPayload1);
-        var tempPayload2 = $(`#payLoadB${i}`).val();
-        console.log(tempPayload2);
-        payLoad[i] = `${tempPayload1} ${tempPayload2}`
-        }
-    })
+            var tempPayload1 = $(`#payLoadA${i}`).val();
+            console.log(tempPayload1);
+            var tempPayload2 = $(`#payLoadB${i}`).val();
+            console.log(tempPayload2);
+            payLoad[i] = `${tempPayload1} ${tempPayload2}`
+            }
+        })
 
 
 
