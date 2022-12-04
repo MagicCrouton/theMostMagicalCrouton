0f59f92a5965d38decd55963547a0a92c4adb2c6
Eugene Park <e.park5336@gmail.com>
2022-12-04 02:57:54 -0500

    added secondary api function to help shape the captions of the tiles better

--- assets/storyData.js
+++ assets/storyData.js
@@ -1,4 +1,4 @@
-var artStyles = ['expressionism','popArt','baroque','post-Impressionism','street Art','surrealism'];
+var artStyles = ['comicbook style','popArt','baroque','street Art','surrealism'];
 var subStyle = ['cyberpunk', 'steampunk', 'synthwave', 'hand Drawn','photorealistic',]
 
 var randomGen = Math.floor(Math.random() * artStyles.length);
@@ -10,31 +10,31 @@ var style = subStyle[randomGen];
 
 
 var story1 = {
-    narration: [`The most Magical Crouton brings a * to slay a*`,
-    `he thinks about his * and decides to*`,
+    narration: [`The most Magical Crouton has decided to fight a * with a*`,
+    `he remembers his * and decides to *`,
     `suddenly a * burst through to door with a*`,
-    `the most Magical Crouton * everyone is *`],
+    `the most Magical Crouton thinks * everyone watching is *`],
      payLoadGuide:[
-    ['a Magical Crouton holding a','*','to slay the', '*'],
-    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a Magical Crouton fights a','*','holding a', '*'],
+    ['a Magical Crouton remebers his','*','and decides to','*'],
     ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
-    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ['a magical crouton is thinking','*','and a crowd of people around looking at the crouton is','*']
     ],
      style:`a ${style} ${art} of`
 };
 var story2 = {
-    narration: [`The most Magical Crouton brings a * to slay a*`,
-    `he thinks about his * and decides to*`,
-    `suddenly a * burst through to door with a*`,
-    `the most Magical Crouton * everyone is *`],
+    narration: [`You are driving a*and suddenly you see a critter that looks like a*`,
+    `you *and you hope*`,
+    `the critter grabs a*and*`,
+    `the critter*and everyone is*`],
      payLoadGuide:[
-    ['a Magical Crouton holding a','*','to slay the', '*'],
-    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a first person view of a person driving a','*','and suddenly seeing a', '*', 'on the road'],
+    ['a person','*','and praying about','*', "but you don't hear a thud"],
     ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
-    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ['a magical crouton is thinking','*','and a crowd of people around looking at the crouton is','*']
     ],
      style:`a ${style} ${art} of`
-};
+}
 // var story2 = {
 //     narration: [`You are driving a*and suddenly you see a critter that looks like a*`,
 //     `you close your*and you hope*`,
@@ -46,28 +46,28 @@ var story2 = {
 //      tile4:'Magical Crouton holding a',
 // };
 var story3 = {
-    narration: [`The most Magical Crouton brings a * to slay a*`,
-    `he thinks about his * and decides to*`,
+    narration: [`The most Magical Crouton has decided to fight a * with a*`,
+    `he remembers his * and decides to *`,
     `suddenly a * burst through to door with a*`,
-    `the most Magical Crouton * everyone is *`],
+    `the most Magical Crouton thinks * everyone watching is *`],
      payLoadGuide:[
-    ['a Magical Crouton holding a','*','to slay the', '*'],
-    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a Magical Crouton fights a','*','holding a', '*'],
+    ['a Magical Crouton remebers his','*','and decides to','*'],
     ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
-    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ['a magical crouton is thinking','*','and a crowd of people around looking at the crouton is','*']
     ],
      style:`a ${style} ${art} of`
 };
 var story4 = {
-    narration: [`The most Magical Crouton brings a * to slay a*`,
-    `he thinks about his * and decides to*`,
+    narration: [`The most Magical Crouton has decided to fight a * with a*`,
+    `he remembers his * and decides to *`,
     `suddenly a * burst through to door with a*`,
-    `the most Magical Crouton * everyone is *`],
+    `the most Magical Crouton thinks * everyone watching is *`],
      payLoadGuide:[
-    ['a Magical Crouton holding a','*','to slay the', '*'],
-    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a Magical Crouton fights a','*','holding a', '*'],
+    ['a Magical Crouton remebers his','*','and decides to','*'],
     ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
-    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ['a magical crouton is thinking','*','and a crowd of people around looking at the crouton is','*']
     ],
      style:`a ${style} ${art} of`
 };
