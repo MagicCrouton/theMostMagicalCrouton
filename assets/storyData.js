d3e5256c3b1f667baa2304bd77a5974a91edea6b
Eugene Park <e.park5336@gmail.com>
2022-12-03 21:15:36 -0500

    completed payload generation logic

---
+++ assets/storyData.js
@@ -0,0 +1,73 @@
+var artStyles = ['expressionism','popArt','baroque','post-Impressionism','street Art','surrealism'];
+var subStyle = ['cyberpunk', 'steampunk', 'synthwave', 'hand Drawn','photorealistic',]
+
+var randomGen = Math.floor(Math.random() * artStyles.length);
+var randomArt = Math.floor(Math.random() * artStyles.length);
+var art = artStyles[randomGen];
+var randomGen = Math.floor(Math.random() * subStyle.length);
+var randomStyle = Math.floor(Math.random() * subStyle.length);
+var style = subStyle[randomGen];
+
+
+var story1 = {
+    narration: [`The most Magical Crouton brings a * to slay a*`,
+    `he thinks about his * and decides to*`,
+    `suddenly a * burst through to door with a*`,
+    `the most Magical Crouton * everyone is *`],
+     payLoadGuide:[
+    ['a Magical Crouton holding a','*','to slay the', '*'],
+    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
+    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ],
+     style:`a ${style} ${art} of`
+};
+var story2 = {
+    narration: [`The most Magical Crouton brings a * to slay the*`,
+    `he thinks about his * and decides to*`,
+    `suddenly a * burst through to door with a*`,
+    `the most Magical Crouton * everyone is *`],
+     payLoadGuide:[
+    ['a Magical Crouton holding a','*','to slay a', '*'],
+    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
+    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ],
+     style:`a ${style} ${art} of`
+};
+// var story2 = {
+//     narration: [`You are driving a*and suddenly you see a critter that looks like a*`,
+//     `you close your*and you hope*`,
+//     `the critter grabs a*and*`,
+//     `the critter*and everyone is*`],
+//      tile1: 'Magical Crouton holding a',
+//      tile2:'Magical Crouton holding a',
+//      tile3:'Magical Crouton holding a',
+//      tile4:'Magical Crouton holding a',
+// };
+var story3 = {
+    narration: [`The most Magical Crouton brings a * to slay a*`,
+    `he thinks about his * and decides to*`,
+    `suddenly a * burst through to door with a*`,
+    `the most Magical Crouton * everyone is *`],
+     payLoadGuide:[
+    ['a Magical Crouton holding a','*','to slay a', '*'],
+    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
+    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ],
+     style:`a ${style} ${art} of`
+};
+var story4 = {
+    narration: [`The most Magical Crouton brings a * to slay a*`,
+    `he thinks about his * and decides to*`,
+    `suddenly a * burst through to door with a*`,
+    `the most Magical Crouton * everyone is *`],
+     payLoadGuide:[
+    ['a Magical Crouton holding a','*','to slay the', '*'],
+    ['a Magical Crouton thinking about his','*','holding a', 'thinking about','*'],
+    ['a','*','breakes through the wall holding a','*','to give to a magical crouton'],
+    ['a magical crouton feeling','*','and a crowd of people around looking at the crouton feeling','*']
+    ],
+     style:`a ${style} ${art} of`
+};
