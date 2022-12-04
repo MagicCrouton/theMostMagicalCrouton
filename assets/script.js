563cdc86c6b604be7cb6557065b61c45ec987491
Eugene Park <e.park5336@gmail.com>
2022-12-03 21:15:36 -0500

    completed payload generation logic

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,10 @@ var storySubmitBtnEl = $('#storySubmit');
 var payLoad = [];
 var APIResponse=[];
 var apiKey = `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`
-// API function
 
+// story data has been moved to separate sheet storyData.js
+
+// API function
 function fetchDallE(payLoad, i) {
     fetch("https://api.openai.com/v1/images/generations", {
         method: 'POST',
@@ -33,69 +35,48 @@ function fetchDallE(payLoad, i) {
 
         // use APIResponse.data[0].url to grab image url.
 }
-
 // the fetch function retrieves an image and you need to use APIResponse.data[0].url to grab image url.
-
 // end API function
 
+    var stories = [story1, story2, story3, story4];
+    var randomGen = Math.floor(Math.random() * stories.length);
+    var storyCurrent= stories[randomGen]
+    var storyGen = storyCurrent.narration;
 
 setTimeout(function(){
 
     pageOneEl.attr('class', 'invisible');
     pageTwoEl.removeAttr('class');
-
-    var story1 = [
-    `story1 you find yourself standing in front of * and wanting to`,
-    `you ask * and do.`,
-    `blurb 1 * asdfasdf 2.`,
-    `blub 3 * asdfasdf`
-    ];
-
-    var story2 = [
-        `story2 you find yourself standing in front of * and wanting to`,
-        `you ask * and do.`,
-        `blurb 1 * asdfasdf 2.`,
-        `blub 3 * asdfasdf`
-    ];
-
-    var story3 = [
-        `story3 you find yourself standing in front of * and wanting to`,
-        `you ask * and do.`,
-        `blurb 1 * asdfasdf 2.`,
-        `blub 3 * asdfasdf`
-    ];
-
-    var story4 = [
-        `story4 you find yourself standing in front of * and wanting to`,
-        `you ask * and do.`,
-        `blurb 1 * asdfasdf 2.`,
-        `blub 3 * asdfasdf`
-    ];
-
-    var stories = [story1, story2, story3, story4];
-    var randomGen = Math.floor(Math.random() * 4);
-    var storyGen = stories[randomGen];
-
     for (i=0; i < storyGen.length; ++i){
         var temp =storyGen[i].split("*");
         var tempEl = $(`#story${i+1}`);
         tempEl.text("");
-        tempEl.html(`<span>${temp[0]}<input type="text" id="payLoadA${i}">${temp[1]}<input type="text" id="payLoadB${i}"</span>`)
+        tempEl.html(`
+        <span>
+                 ${temp[0]}<input class="specificInput" type="text" id="payLoadA${i}">${temp[1]}
+                  <input class="specificInput" type="text" id="payLoadB${i}"
+        </span>`)
     }    
 
     var storySubmitBtnEl = $('#storySubmit')
     storySubmitBtnEl.on('click', function(event) {
         event.preventDefault(event);
         for (i=0; i < storyGen.length; ++i) {
-
-            var tempPayload1 = $(`#payLoadA${i}`).val();
-            console.log(tempPayload1);
-            var tempPayload2 = $(`#payLoadB${i}`).val();
-            console.log(tempPayload2);
-            payLoad[i] = `${tempPayload1} ${tempPayload2}`
+            var tempPayload = [];
+            tempPayload[0] = $(`#payLoadA${i}`).val();
+            tempPayload[1] = $(`#payLoadB${i}`).val();
+            var currentTemp = stories[randomGen].payLoadGuide[i];
+            var index = 1;
+                for (n=0; n<2; ++n) {
+                    index = currentTemp.indexOf('*');
+                    currentTemp[index] = ` ${tempPayload[n]} `;
+                }
+            currentTemp = currentTemp.join('');
+            // console.log(currentTemp);
+            payLoad[i] = `${stories[randomGen].style} ${currentTemp}`;
             }
         })
-
+            console.log(payLoad)
 
 
 }, 5000)
