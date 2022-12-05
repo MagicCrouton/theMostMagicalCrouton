4b3ed34cab91b6cb4cbe7358b4e1013693d8db6c
David Orozco <davorozco4@gmail.com>
2022-12-04 20:14:43 -0500

    This is the page before the main

---
+++ assets/imagescript.js
@@ -0,0 +1,12 @@
+var buttonEl = $('#clickMe')
+var hideEl = $('#hideMe')
+var removeEl = $('#hideMeTwo')
+
+
+function removeImage(){
+    removeEl.remove()
+    hideEl.remove()
+    buttonEl.remove()
+ 
+}
+
