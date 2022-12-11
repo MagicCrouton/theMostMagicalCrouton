3c7916136b88ce3f43930dc49436d31771b98158
Eugene Park <e.park5336@gmail.com>
2022-12-10 20:33:59 -0500

    save

--- assets/imagescript.js
+++ assets/imagescript.js
@@ -7,6 +7,11 @@ function removeImage(){
     removeEl.remove()
     hideEl.remove()
     buttonEl.remove()
- 
 }
+
+buttonEl.on('click', function(){
+    if (localStorage.getItem('key') === null) {
+        window.location.href="./APIRegistration.html"
+    }
+})
 
